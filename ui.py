04099c3d326b868b68ae74fb92fecc9c4ccf3974
VarshaVidya <111826112+VarshaVidya@users.noreply.github.com>
2023-04-03 12:36:07 +0800

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,10 @@
+THEME_COLOR = "#375362"
+
+from tkinter import *
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzlet")
+        self.window.config(padx=20,pady=20)
+        self.window.mainloop()
